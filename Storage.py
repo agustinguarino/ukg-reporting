c601a700bd01a2e99fd8c84b064710f8bd59b32e
Agustin Guarino <agustinignacio.guarinobonahon@ukg.com>
2023-12-12 10:37:39 -0300

    Function to get builds in builds file

--- Storage.py
+++ Storage.py
@@ -1,9 +1,15 @@
 import os
 
 class Storage:
+    file_path = ""
+
     def __init__(self):
+        self.file_path = "C:/Data/builds2.txt"
         pass
 
     def buildsFileExists(self):
-        file_path = "C:/Data/builds2.txt"
-        return True if os.path.exists(file_path) else False
+        return True if os.path.exists(self.file_path) else False
+    
+    def getBuildsInBuildsFile(self):
+        with open(self.file_path, "r", encoding="utf-8") as file:
+            return str(file.read).split(",")
