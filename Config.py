db979282655e8b375fdecabbaa9459def1457fb3
Agustin Guarino <agustinignacio.guarinobonahon@ukg.com>
2023-12-12 17:21:21 -0300

    Structure to read csv file

--- Config.py
+++ Config.py
@@ -1,3 +1,16 @@
+import csv
+
 class Config():
+    config_file_path = "Config.csv"
+    config = {}
+
     def __init__(self):
         pass
+
+    def readConfigFile(self):
+        with open(self.config_file_path, mode="r", ) as file:
+            csv_file = csv.reader(file)
+            next(file)
+
+            for line in csv_file:
+                print(line)
