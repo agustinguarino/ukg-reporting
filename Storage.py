f6932b9a8fa9e1bc561b3da6381cf9deb5577c73
Agustin Guarino <agustinignacio.guarinobonahon@ukg.com>
2023-12-12 10:33:49 -0300

    Function to check if builds queue file exists

--- Storage.py
+++ Storage.py
@@ -1,3 +1,9 @@
+import os
+
 class Storage:
     def __init__(self):
         pass
+
+    def buildsFileExists(self):
+        file_path = "C:/Data/builds2.txt"
+        return True if os.path.exists(file_path) else False
