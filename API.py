f401604941e69e72e7d7bef5d00028001be3dfdd
Agustin Guarino <agustinignacio.guarinobonahon@ukg.com>
2023-12-18 10:27:21 -0300

    Function to get last 5 builds in P0 pipeline

--- API.py
+++ API.py
@@ -1,3 +1,12 @@
+import requests
+from Config import Config
+
 class API:
+    config = ""
+
     def __init__(self):
-        pass
+        self.config = Config().getConfig()
+
+    def getBuildConsoleBuilds(self):
+        url = self.config["bc_pipeline_api_url"]
+        return requests.get(url).json()
