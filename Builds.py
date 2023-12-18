c9bf97d3e3e50aab512d4abdda4b80bf61dd7e9c
Agustin Guarino <agustinignacio.guarinobonahon@ukg.com>
2023-12-18 11:30:25 -0300

    Initial structure

---
+++ Builds.py
@@ -0,0 +1,15 @@
+class Builds:
+    payload = ""
+    data = {}
+
+    def __init_(self, payload):
+        self.payload = payload
+
+    def getInformation(self):
+        return self.data
+    
+    def analyzeBuildConsoleGeneralBuilds(self):
+        pass
+    
+    def analyzeBuildConsoleSpecificBuild(self):
+        pass
