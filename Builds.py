0b826f6c3b3f89c3380ce5f5352fe0e3843d8906
Agustin Guarino <agustinignacio.guarinobonahon@ukg.com>
2023-12-18 12:08:42 -0300

    Analyze pipeline and specific build payloads and return information

--- Builds.py
+++ Builds.py
@@ -1,15 +1,29 @@
 class Builds:
     payload = ""
-    data = {}
+    information = {}
 
-    def __init_(self, payload):
+    def __init__(self, payload):
         self.payload = payload
 
     def getInformation(self):
-        return self.data
+        return self.information
     
     def analyzeBuildConsoleGeneralBuilds(self):
-        pass
-    
+        data = self.payload["builds"]
+        builds_amount = len(data)
+
+        for i in range(0, builds_amount):
+            build_id = data[i]["_id"]
+            state = data[i]["displayStepMap"]["P0 Quality Gate"]["subSteps"][0]["progress"]["state"]
+
+            self.information[build_id] = state
+
     def analyzeBuildConsoleSpecificBuild(self):
-        pass
+        data = self.payload
+        build_id = data["_id"]
+        build_state = data["displayStepMap"]["P0 Quality Gate"]["subSteps"][0]["progress"]["state"]
+        teamcity_id = data["displayStepMap"]["P0 Quality Gate"]["subSteps"][0]["progress"]["id"]
+
+        self.information["build_id"] = build_id
+        self.information["build_state"] = build_state
+        self.information["teamcity_id"] = teamcity_id
