6a0ef2ff206eb9ddfa861d952df44e824c7f52a3
Agustin Guarino <agustinignacio.guarinobonahon@ukg.com>
2023-12-18 10:32:13 -0300

    Function to get information from specific build in buildconsole P0 pipeline

--- API.py
+++ API.py
@@ -10,3 +10,7 @@ class API:
     def getBuildConsoleBuilds(self):
         url = self.config["bc_pipeline_api_url"]
         return requests.get(url).json()
+    
+    def getBuildConsoleSpecificBuild(self, build_id):
+        url = self.config["bc_build_api_url"] + str(build_id)
+        return requests.get(url).json()
